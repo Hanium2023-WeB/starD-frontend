068d5c62ccf5cacaf3a23f145a682bdd8118f277
jiruen <kjr2699@naver.com>
2025-01-07 02:09:24 +0900

    [#136] 로그아웃 시 sse 알림 구독 해제

--- src/pages/userpage/Logout.js
+++ src/pages/userpage/Logout.js
@@ -1,48 +1,54 @@
 import axios from "axios";
 import {useNavigate} from 'react-router-dom';
-import {useEffect, useState} from "react";
+import {useEffect} from "react";
+import {useEventSource} from "../notification/EventSourceContext";
 
 const Logout = ({sideheader}) => {
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
+  const { closeEventSource, setAccessToken } = useEventSource();
+  
+  useEffect(() => {
+    const member = localStorage.getItem('isLoggedInUserId');
+    const accessToken = localStorage.getItem('accessToken');
 
-    useEffect(() => {
-        const member = localStorage.getItem('isLoggedInUserId');
-        const accessToken = localStorage.getItem('accessToken');
 
-        if (member && accessToken) {
-            axios.post("/api/members/auth/sign-out",{}, {
-                withCredentials: true,
-                headers: {
-                    'Authorization': `Bearer ${accessToken}`
-                }
-            }).then((res) => {
-                console.log("로그아웃 성공");
-                localStorage.removeItem('accessToken');
-                localStorage.removeItem('isLoggedInUserId');
-                localStorage.removeItem('todos');
-                localStorage.removeItem('ApplyStudies');
-                localStorage.removeItem('selectedSido');
-                localStorage.removeItem('selectedGugun');
-                localStorage.removeItem('tags');
-                localStorage.removeItem('studies');
-
-                alert("로그아웃 성공");
-                navigate('/');
-            })
-                .catch((error) => {
-                    console.log("로그아웃 실패" + error);
-                    navigate('/');
-                });
-        } else {
-            console.log("로그아웃 실패 (로그인 하지 않는 상태)");
-            alert("로그인이 필요합니다.");
-            navigate("/login");
+    if (member && accessToken) {
+      axios.post("/api/members/auth/sign-out", {}, {
+        withCredentials: true,
+        headers: {
+          'Authorization': `Bearer ${accessToken}`
         }
-    }, []);
-    return (
-        null
-    );
+      }).then((res) => {
+        console.log("로그아웃 성공");
+        localStorage.removeItem('accessToken');
+        localStorage.removeItem('isLoggedInUserId');
+        localStorage.removeItem('todos');
+        localStorage.removeItem('ApplyStudies');
+        localStorage.removeItem('selectedSido');
+        localStorage.removeItem('selectedGugun');
+        localStorage.removeItem('tags');
+        localStorage.removeItem('studies');
+
+        closeEventSource();
+        setAccessToken(null);
+
+        alert("로그아웃 성공");
+        navigate('/');
+      })
+      .catch((error) => {
+        console.log("로그아웃 실패" + error);
+        navigate('/');
+      });
+    } else {
+      console.log("로그아웃 실패 (로그인 하지 않는 상태)");
+      alert("로그인이 필요합니다.");
+      navigate("/login");
+    }
+  }, []);
+  return (
+      null
+  );
 }
 
 export default Logout;
