13c5b1b82a9d4351a96b0ba14b377f9f427a00a5
jiruen <kjr2699@naver.com>
2025-01-06 13:19:47 +0900

    [#135] 비회원인 경우 댓글 신고 버튼 보이지 않도록 수정

--- src/components/comment/CommentList.js
+++ src/components/comment/CommentList.js
@@ -14,11 +14,12 @@ const formatDatetime = (datetime) => {
   const formattedDatetime = `${year}-${month}-${day} ${hours}:${minutes}`;
   return formattedDatetime;
 };
+
 const CommentList = ({ comments, onEditClick, onRemoveClick, onReplySubmit }) => {
   const [showReportModal, setShowReportModal] = useState(false);
   const [reportCommentId, setReportCommentId] = useState(null);
   const navigate = useNavigate();
-
+  const accessToken = localStorage.getItem('accessToken');
   const handleOpenReportModal = (commentId) => {
     setReportCommentId(commentId);
     setShowReportModal(true);
@@ -38,61 +39,65 @@ const CommentList = ({ comments, onEditClick, onRemoveClick, onReplySubmit }) =>
   }
 
   return (
-    <div className="comment_list">
-      <ul>
-        {comments.map((comment, index) => (
-            <li key={index} className="comment" >
-              <div style={{display:"flex", justifyContent:"space-between"}}>
-                <div className="comment_profile">
-                  <ImageComponent getImgName = {comment.profileImg} imageSrc={""} />
-                  <Link to={`/${comment.writer}/profile`}
-                        style={{
-                          textDecoration: "none",
-                          color: "inherit",
-                        }}>
-                    <strong>{comment.writer}</strong>
-                  </Link>
-                </div>
-                <div>
-                  {comment.isAuthor && (
-                      <>
-                        <span className="comment_edit_btn" onClick={() => onEditClick(comment.replyId)}>
+      <div className="comment_list">
+        <ul>
+          {comments.map((comment, index) => (
+              <li key={index} className="comment">
+                <div style={{display: "flex", justifyContent: "space-between"}}>
+                  <div className="comment_profile">
+                    <ImageComponent getImgName={comment.profileImg}
+                                    imageSrc={""}/>
+                    <Link to={`/${comment.writer}/profile`}
+                          style={{
+                            textDecoration: "none",
+                            color: "inherit",
+                          }}>
+                      <strong>{comment.writer}</strong>
+                    </Link>
+                  </div>
+                  <div>
+                    {comment.isAuthor && (
+                        <>
+                        <span className="comment_edit_btn"
+                              onClick={() => onEditClick(comment.replyId)}>
                           수정
                         </span>
-                        <span>&nbsp;&nbsp; | &nbsp;&nbsp;</span>
-                        <span className="comment_remove_btn" onClick={() => onRemoveClick(comment.replyId)}>
+                          <span>&nbsp;&nbsp; | &nbsp;&nbsp;</span>
+                          <span className="comment_remove_btn"
+                                onClick={() => onRemoveClick(comment.replyId)}>
                           삭제
                         </span>
-                      </>
-                  )}
+                        </>
+                    )}
+                  </div>
                 </div>
-              </div>
-              <p>{comment.content}</p>
-              <span>{formatDatetime(comment.createdAt)}</span>
-              {comment.createdAt !== comment.updatedAt && (
-                  <>
-                    <span>&nbsp;&nbsp;&nbsp;&nbsp;</span>
-                    <span>( 수정: {formatDatetime(comment.updatedAt)} )</span>
-                  </>
-              )}
-              {!comment.isAuthor && (
-                  <>
-                    <span>&nbsp;&nbsp; | &nbsp;&nbsp;</span>
-                    <span className="comment_report_btn"
-                          onClick={() => handleOpenReportModal(comment.replyId)}>신고</span>
-                  </>
-              )}
-            </li>
-        ))}
-      </ul>
-      <Report
-          show={showReportModal}
-          handleClose={handleCloseReportModal}
-          onReportSubmit={handleReportSubmit}
-          targetId={reportCommentId}
-          targetType={"reply"}
-      />
-    </div>
+                <p>{comment.content}</p>
+                <span>{formatDatetime(comment.createdAt)}</span>
+                {comment.createdAt !== comment.updatedAt && (
+                    <>
+                      <span>&nbsp;&nbsp;&nbsp;&nbsp;</span>
+                      <span>( 수정: {formatDatetime(comment.updatedAt)} )</span>
+                    </>
+                )}
+                {!comment.isAuthor && accessToken && (
+                    <>
+                      <span>&nbsp;&nbsp; | &nbsp;&nbsp;</span>
+                      <span className="comment_report_btn"
+                            onClick={() => handleOpenReportModal(
+                                comment.replyId)}>신고</span>
+                    </>
+                )}
+              </li>
+          ))}
+        </ul>
+        <Report
+            show={showReportModal}
+            handleClose={handleCloseReportModal}
+            onReportSubmit={handleReportSubmit}
+            targetId={reportCommentId}
+            targetType={"reply"}
+        />
+      </div>
   );
 };
 
