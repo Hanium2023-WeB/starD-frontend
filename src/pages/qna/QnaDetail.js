d047d3cb7622ce60e6563407288ca11ab98db9d1
jiruen <kjr2699@naver.com>
2025-01-10 17:11:41 +0900

    [#147] QNA 상세 페이지 수정

--- src/pages/qna/QnaDetail.js
+++ src/pages/qna/QnaDetail.js
@@ -9,245 +9,250 @@ import default_profile_img from "../../images/default_profile_img.png";
 import axiosInstance from "../../api/axiosInstance";
 
 const QnaDetail = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const {id} = useParams();
-    console.log("postId : ", id);
-    const location = useLocation();
-    const { postType } = location.state || {};
-    console.log(postType);
+  const {id} = useParams();
+  const location = useLocation();
+  const {postType} = location.state || {};
 
-    const [postItem, setPostItem] = useState(null);
+  const [postItem, setPostItem] = useState(null);
 
-    const [posts, setPosts] = useState([]);
-    const [editing, setEditing] = useState(false);
-    const [postDetail, setPostDetail] = useState([]);
+  const [posts, setPosts] = useState([]);
+  const [editing, setEditing] = useState(false);
+  const [postDetail, setPostDetail] = useState([]);
 
-    let accessToken = localStorage.getItem('accessToken');
-    let isLoggedInUserId = localStorage.getItem('isLoggedInUserId');
-    const [url, setUrl] = useState(null);
-    const [initiallyUrlStates, setInitiallyUrlStates] = useState(false);
+  let accessToken = localStorage.getItem('accessToken');
+  let isLoggedInUserId = localStorage.getItem('isLoggedInUserId');
+  const [url, setUrl] = useState(null);
+  const [initiallyUrlStates, setInitiallyUrlStates] = useState(false);
 
-    const [isWriter, setIsWriter] = useState(false);
-    const [isAdmin, setIsAdmin] = useState(false);
+  const [isWriter, setIsWriter] = useState(false);
+  const [isAdmin, setIsAdmin] = useState(false);
 
-    useEffect(() => {
-        if (postType === "FAQ") {
-            setUrl(`/api/faqs/${id}`);
-        }
-        else if (postType === "QNA") {
-            setUrl(`/api/qnas/${id}`);
-        }
-
-        setInitiallyUrlStates(true);
-    }, [id]);
-
-    useEffect(() => {
-        axiosInstance
-            .get("/members/auth")
-            .then((res) => {
-                const auth = res.data;
-
-                if (auth === "USER") {
-                    setIsAdmin(false);
-                }
-                else if (auth === "ADMIN") {
-                    setIsAdmin(true);
-                }
-            })
-            .catch((error) => {
-                console.error("권한 조회 실패:", error);
-                setIsAdmin(false);
-            });
-    }, [accessToken]);
-
-    useEffect(() => {
-        const config = {
-            headers: {}
-        };
-
-        if (accessToken && isLoggedInUserId) {
-            config.headers['Authorization'] = `Bearer ${accessToken}`;
-        }
-
-        if (initiallyUrlStates) {
-            axios.get(url, config)
-                .then((res) => {
-                    console.log(res.data);
-                    setPostItem(res.data);
-                    if (res.data.isAuthor) { // 자신의 글인지
-                        setIsWriter(true);
-                    }
-                })
-                .catch((error) => {
-                    console.error("qna 세부 데이터 가져오기 실패:", error);
-                });
-        }
-    }, [id, accessToken, isLoggedInUserId, initiallyUrlStates]);
-
-    const handleEditClick = () => {
-        setEditing(true);
+  useEffect(() => {
+    if (postType === "FAQ") {
+      setUrl(`/api/faqs/${id}`);
+    } else if (postType === "QNA") {
+      setUrl(`/api/qnas/${id}`);
     }
 
-    const handleCancelEdit = () => {
-        setEditing(false);
-    }
+    setInitiallyUrlStates(true);
+  }, [id]);
 
-    const handlePostUpdate = (updatedPost) => {
-        console.log("수정 예정:", updatedPost.postId, updatedPost.title, updatedPost.content, updatedPost.postType);
+  useEffect(() => {
+    if (accessToken) {
+      axiosInstance
+      .get("/members/auth")
+      .then((res) => {
+        const auth = res.data;
 
-        // Authorization 헤더 구성
-        const config = {
-            headers: {}
-        };
-        if (accessToken && isLoggedInUserId) {
-            config.headers['Authorization'] = `Bearer ${accessToken}`;
+        if (auth === "USER") {
+          setIsAdmin(false);
+        } else if (auth === "ADMIN") {
+          setIsAdmin(true);
         }
+      })
+      .catch((error) => {
+        console.error("권한 조회 실패:", error);
+        setIsAdmin(false);
+      });
+    }
+  }, [accessToken]);
 
-        // 수정 요청 보내기
-        axios
-            .put(
-                url, // API URL
-                { // 전달할 데이터
-                    title: updatedPost.title,
-                    content: updatedPost.content,
-                },
-                config // 헤더 설정
-            )
-            .then(response => {
-                console.log("qna 수정 성공:", response.data);
-                alert("게시글이 수정되었습니다.");
-                setPostItem(response.data)
-                setEditing(false); // 수정 모드 비활성화
-            })
-            .catch(error => {
-                console.error("qna 수정 실패:", error.response || error.message);
-                alert("수정에 실패했습니다.");
-            });
+  useEffect(() => {
+    if (initiallyUrlStates) {
+      axios.get(url, {
+        withCredentials: true,
+        headers: accessToken ? {'Authorization': `Bearer ${accessToken}`} : {},
+      })
+      .then((res) => {
+        setPostItem(res.data);
+        if (res.data.isAuthor) { // 자신의 글인지
+          setIsWriter(true);
+        }
+      })
+      .catch((error) => {
+        console.error("qna 세부 데이터 가져오기 실패:", error);
+      });
+    }
+  }, [id, accessToken, isLoggedInUserId, initiallyUrlStates]);
+
+  const handleEditClick = () => {
+    setEditing(true);
+  }
+
+  const handleCancelEdit = () => {
+    setEditing(false);
+  }
+
+  const handlePostUpdate = (updatedPost) => {
+    console.log("수정 예정:", updatedPost.postId, updatedPost.title,
+        updatedPost.content, updatedPost.postType);
+
+    // Authorization 헤더 구성
+    const config = {
+      headers: {}
     };
-
-    const handlePostDelete = () => {
-        const confirmDelete = window.confirm("정말로 게시글을 삭제하시겠습니까?");
-        if (confirmDelete) {
-            axios.delete(url, {
-                withCredentials: true,
-                headers: {
-                    'Authorization': `Bearer ${accessToken}`
-                }
-            })
-                .then(response => {
-                    console.log("qna 삭제 성공 ");
-                    alert("게시글이 삭제되었습니다.");
-                    const updatedPosts = posts.filter(post => post.postId !== postDetail[0].postId);
-                    setPosts(updatedPosts);
-                    navigate("/qna/page=1");
-                })
-                .catch(error => {
-                    console.error("Error:", error);
-                    console.log("qna 삭제 실패");
-
-                    alert("삭제에 실패했습니다.");
-                });
-        }
+    if (accessToken && isLoggedInUserId) {
+      config.headers['Authorization'] = `Bearer ${accessToken}`;
     }
 
-    const formatDatetime = (datetime) => {
-        const date = new Date(datetime);
-        const year = date.getFullYear();
-        const month = String(date.getMonth() + 1).padStart(2, "0");
-        const day = String(date.getDate()).padStart(2, "0");
-        const hours = String(date.getHours()).padStart(2, "0");
-        const minutes = String(date.getMinutes()).padStart(2, "0");
-        const formattedDatetime = `${year}-${month}-${day} ${hours}:${minutes}`;
-        return formattedDatetime;
-    };
+    // 수정 요청 보내기
+    axios
+    .put(
+        url, // API URL
+        { // 전달할 데이터
+          title: updatedPost.title,
+          content: updatedPost.content,
+        },
+        config // 헤더 설정
+    )
+    .then(response => {
+      console.log("qna 수정 성공:", response.data);
+      alert("게시글이 수정되었습니다.");
+      setPostItem(response.data)
+      setEditing(false); // 수정 모드 비활성화
+    })
+    .catch(error => {
+      console.error("qna 수정 실패:", error.response || error.message);
+      alert("수정에 실패했습니다.");
+    });
+  };
 
-    return (
-        <div>
-            <Header showSideCenter={true}/>
-            <div className="community_container">
-                <Backarrow subname={"QNA LIST"}/>
-                {editing ? (
-                    <QnaEdit
-                        post={postItem}
-                        onUpdatePost={handlePostUpdate}
-                        onCancel={handleCancelEdit}
-                    />
-                ) : (
-                    <div className="community_detail">
-                        {postItem && (
-                            <div className="post_header">
-                                <div className="post_category">
-                                    <span>카테고리 > </span>
-                                    <span>{postItem.postType}</span>
-                                </div>
-                                <div style={{display:"flex", justifyContent:"space-between"}}>
-                                    <div className="post_title">
-                                        {postItem.title}
-                                    </div>
-                                    {(isWriter || (isWriter && isAdmin)) && (
-                                        <div className="button">
-                                            <button style={{marginRight:"5px"}} onClick={handleEditClick}>수정</button>
-                                            <button onClick={handlePostDelete}>삭제</button>
-                                        </div>
-                                    )}
-                                    {(isAdmin && !isWriter) && (
-                                        <div className="button">
-                                            <button onClick={handlePostDelete}>삭제</button>
-                                        </div>
-                                    )}
-                                </div>
-                                <div className="post_info">
-                                    <div className="left">
-                                        {postItem.type === "FAQ" ? (
-                                            <td className="community_nickname">관리자</td>
-                                        ) : (
-                                            <td className="community_nickname">
-                                                <img
-                                                    src={postItem.profileImg || default_profile_img} // 기본 이미지 경로
-                                                    alt="프로필 이미지"
-                                                    className="profile_image" // 필요한 경우 CSS 클래스 추가
-                                                />
-                                                {postItem.writer}
-                                            </td>
-                                        )}
-                                        <span className="post_created_date">{formatDatetime(postItem.createdAt)}</span>
-                                        {postItem.createdAt !== postItem.updatedAt && (
-                                            <>
-                                                <span>&nbsp;&nbsp;&nbsp;</span>
-                                                <span>( 수정: {formatDatetime(postItem.updatedAt)} )</span>
-                                            </>
-                                        )}
-                                    </div>
-                                    <div className="right">
-                                        <span className="hit_count">조회 <span>{postItem.hit}</span></span>
-                                    </div>
-                                </div>
+  const handlePostDelete = () => {
+    const confirmDelete = window.confirm("정말로 게시글을 삭제하시겠습니까?");
+    if (confirmDelete) {
+      axios.delete(url, {
+        withCredentials: true,
+        headers: {
+          'Authorization': `Bearer ${accessToken}`
+        }
+      })
+      .then(response => {
+        console.log("qna 삭제 성공 ");
+        alert("게시글이 삭제되었습니다.");
+        const updatedPosts = posts.filter(
+            post => post.postId !== postDetail[0].postId);
+        setPosts(updatedPosts);
+        navigate("/qna/page=1");
+      })
+      .catch(error => {
+        console.error("Error:", error);
+        console.log("qna 삭제 실패");
+
+        alert("삭제에 실패했습니다.");
+      });
+    }
+  }
+
+  const formatDatetime = (datetime) => {
+    const date = new Date(datetime);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const day = String(date.getDate()).padStart(2, "0");
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const formattedDatetime = `${year}-${month}-${day} ${hours}:${minutes}`;
+    return formattedDatetime;
+  };
+
+  return (
+      <div>
+        <Header showSideCenter={true}/>
+        <div className="community_container">
+          <Backarrow subname={"QNA LIST"}/>
+          {editing ? (
+              <QnaEdit
+                  post={postItem}
+                  onUpdatePost={handlePostUpdate}
+                  onCancel={handleCancelEdit}
+              />
+          ) : (
+              <div className="community_detail">
+                {postItem && (
+                    <div className="post_header">
+                      <div className="post_category">
+                        <span>카테고리 > </span>
+                        <span>{postItem.postType}</span>
+                      </div>
+                      <div style={{
+                        display: "flex",
+                        justifyContent: "space-between"
+                      }}>
+                        <div className="post_title">
+                          {postItem.title}
+                        </div>
+                        {(isWriter || (isWriter && isAdmin)) && (
+                            <div className="button">
+                              <button style={{marginRight: "5px"}}
+                                      onClick={handleEditClick}>수정
+                              </button>
+                              <button onClick={handlePostDelete}>삭제</button>
                             </div>
                         )}
-                        {postItem && (
-                            <div className="post_content" dangerouslySetInnerHTML={{ __html: postItem.content.replace(/\n/g, '<br>') }} />
+                        {(isAdmin && !isWriter) && (
+                            <div className="button">
+                              <button onClick={handlePostDelete}>삭제</button>
+                            </div>
                         )}
-
-                        <div className="btn">
-                            <Link to={"/qna/page=1"}
-                                  style={{
-                                      textDecoration: "none",
-                                      color: "inherit",
-                                  }}
-                            >
-                                <button className="community_list_btn">글 목록보기</button>
-                            </Link>
+                      </div>
+                      <div className="post_info">
+                        <div className="left">
+                          {postItem.type === "FAQ" ? (
+                              <td className="community_nickname">관리자</td>
+                          ) : (
+                              <td className="community_nickname">
+                                <img
+                                    src={postItem.profileImg
+                                        || default_profile_img} // 기본 이미지 경로
+                                    alt="프로필 이미지"
+                                    className="profile_image" // 필요한 경우 CSS 클래스 추가
+                                />
+                                {postItem.writer}
+                              </td>
+                          )}
+                          <span className="post_created_date">{formatDatetime(
+                              postItem.createdAt)}</span>
+                          {postItem.createdAt !== postItem.updatedAt && (
+                              <>
+                                <span>&nbsp;&nbsp;&nbsp;</span>
+                                <span>( 수정: {formatDatetime(
+                                    postItem.updatedAt)} )</span>
+                              </>
+                          )}
                         </div>
+                        <div className="right">
+                          <span
+                              className="hit_count">조회 <span>{postItem.hit}</span></span>
+                        </div>
+                      </div>
                     </div>
                 )}
-            </div>
-            {postItem && postItem.type === "QNA" && (
-                <div className="comment_container">
-                    <Comment type="qna" />
+                {postItem && (
+                    <div className="post_content" dangerouslySetInnerHTML={{
+                      __html: postItem.content.replace(/\n/g, '<br>')
+                    }}/>
+                )}
+
+                <div className="btn">
+                  <Link to={"/qna/page=1"}
+                        style={{
+                          textDecoration: "none",
+                          color: "inherit",
+                        }}
+                  >
+                    <button className="community_list_btn">글 목록보기</button>
+                  </Link>
                 </div>
-            )}
+              </div>
+          )}
         </div>
-    )
+        {postItem && postItem.postType === "QNA" && (
+            <div className="comment_container">
+              <Comment type="qna"/>
+            </div>
+        )}
+      </div>
+  )
 }
 export default QnaDetail;
