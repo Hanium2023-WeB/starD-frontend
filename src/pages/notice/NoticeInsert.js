b7f2f3d72f8fc39edb11eb987b3e1012f0df310e
jiruen <kjr2699@naver.com>
2025-01-12 18:22:47 +0900

    [#147] 공지사항 페이지 수정

--- src/pages/notice/NoticeInsert.js
+++ src/pages/notice/NoticeInsert.js
@@ -1,15 +1,18 @@
-import React, {useCallback, useState} from "react";
+import React, {useCallback, useRef, useState} from "react";
 import axios from "axios";
 import Header from "../../components/repeat_etc/Header";
 import Backarrow from "../../components/repeat_etc/Backarrow";
+import toast from "react-hot-toast";
 
 const NoticeInsert = () => {
     const [dataId, setDataId] = useState(0);
     const [posts, setPosts] = useState([]);
+    const titleRef = useRef();
+    const contentRef = useRef();
     const [formData, setFormData] = useState({
-        title:"",
-        content:"",
-        created_date:new Date(),
+        title: "",
+        content: "",
+        created_date: new Date(),
     })
 
     const handleInputChange = (e) => {
@@ -43,29 +46,26 @@ const NoticeInsert = () => {
     const handleSubmit = useCallback(e => {
         e.preventDefault();
 
-        if (
-            formData.title.trim() === '' &&
-            formData.content.trim() === ''
-        ) {
-            alert('게시글 정보를 입력해주세요.');
-            return;
-        }
         if (formData.title.trim() === '') {
             alert("제목을 입력해주세요.");
+            titleRef.current.focus();
             return;
         }
+
         if (formData.content.trim() === '') {
             alert("내용을 입력해주세요.");
+            contentRef.current.focus();
             return;
         }
+
         setFormData(onInsertPost(formData));
-        
+
         const accessToken = localStorage.getItem('accessToken');
 
         const response = axios.post("/api/notices",
             {
-                title:formData.title,
-                content:formData.content,
+                title: formData.title,
+                content: formData.content,
             },
             {
                 withCredentials: true,
@@ -92,27 +92,27 @@ const NoticeInsert = () => {
                 <div className="community_container">
                     <p id={"entry-path"}> 홈 > Notice </p>
                     <Backarrow subname={"Notice Insert"}/>
-        <form className="new_post_form" onSubmit={handleSubmit}>
-            <div style={{display:"flex"}}>
-                <span style={{paddingLeft: "10px",marginTop:"25px"}}>제목</span>
-                <input type="text" name="title" value={formData.title} onChange={handleInputChange}/>
-            </div>
-            <div>
-                <span>카테고리</span>
-                <span className="field_wrapper">
+                    <form className="new_post_form" onSubmit={handleSubmit}>
+                        <div style={{display: "flex"}}>
+                            <span style={{paddingLeft: "10px", marginTop: "25px"}}>제목</span>
+                            <input ref={titleRef} type="text" name="title" value={formData.title} onChange={handleInputChange}/>
+                        </div>
+                        <div>
+                            <span>카테고리</span>
+                            <span className="field_wrapper">
                     <select name="category" onChange={handleInputChange} disabled>
                         <option value="default">공지</option>
                     </select>
                 </span>
-            </div>
-            <div style={{display:"flex"}}>
-                <span style={{paddingLeft: "10px",marginTop:"5px"}}>상세 내용</span>
-                <textarea name="content" value={formData.content} onChange={handleInputChange}/>
-            </div>
-            <div className="btn">
-                <input type="submit" value="등록하기" className="register_btn"/>
-            </div>
-        </form>
+                        </div>
+                        <div style={{display: "flex"}}>
+                            <span style={{paddingLeft: "10px", marginTop: "5px"}}>상세 내용</span>
+                            <textarea ref={contentRef} name="content" value={formData.content} onChange={handleInputChange}/>
+                        </div>
+                        <div className="btn">
+                            <input type="submit" value="등록하기" className="register_btn"/>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
